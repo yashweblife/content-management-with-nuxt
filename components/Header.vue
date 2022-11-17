c6c6254c8e48210b1930f5d11339fcf8b6356c03
yashweblife <yashweblife20@gmail.com>
2022-11-17 14:31:18 -0600

    Added header component

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>
+            Nuxt Posts | {{title}}
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/users">Users</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup lang="ts">
+    const {title} = defineProps(['title'])
+</script>
+
+<style scoped>
+
+</style>
