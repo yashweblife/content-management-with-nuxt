c0458399b154c0834911f6a23333bab8d264c56c
yashweblife <yashweblife20@gmail.com>
2022-11-18 16:03:10 -0600

    Added user card

---
+++ components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <NuxtLink class="card" :to="`users/${'' + user.id}`">
+        <div class="header">
+            <div class="image"></div>
+            <div class="name">
+                {{ user.name }}
+            </div>
+        </div>
+        <div class="content">
+            <div class="info">
+                <p>{{ user.company.name }}</p>
+                <p>{{ user.company.bs }}</p>
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const { user } = defineProps(["user"])
+</script>
+
+<style scoped>
+
+</style>
